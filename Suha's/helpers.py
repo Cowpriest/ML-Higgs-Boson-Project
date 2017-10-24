b2837d6402717839822caf04d4503d35b280ec4e
kagankose <skagankose@Suha-Kagans-Macbook.local>
2017-10-24 12:20:39 +0200

    LR w/ Accuracy .82

--- Suha's/helpers.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:003fd5637c089b8c7b010b2944528afb311e9dd16071ee04518e34b654bf93b5
-size 3838
--- Suha's/main.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c6d182189d05ca0cc80f25929ea63c9768e057b6e5d981b9be6fac0f16de9d57
-size 7151
