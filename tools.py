9316e27ed7fc9a2d7adff180599c638a82db6d51
xiangzhemeng <xiangzhemeng@tsf-444-wpa-0-070.epfl.ch>
2017-10-26 21:49:20 +0200

    update notebook, add comments to tool.py

--- tools.py
+++ tools.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3e006be27ba1987302ed01db72d9bf9009d8b5babe7bea6fefb35a960ea37a90
-size 3856
+oid sha256:6f11e228fa1a1638cca6ea21efdf6017a6d3e800d7965eca4a64940e281b54da
+size 4159
